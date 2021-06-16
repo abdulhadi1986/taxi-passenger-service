71b5d3d694e46062e1b2dbf0f96a112ed6c043d8
Abdulhadi.Abulzahab <abdulhadi.abulzahab@rabobank.nl>
2021-06-16 16:25:40 +0200

    init

---
+++ src/main/java/com/abdulraham/taxipassenger/TaxiPassengerApplication.java
@@ -0,0 +1,13 @@
+package com.abdulraham.taxipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiPassengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiPassengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdulraham/taxipassenger/TaxiPassengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulraham.taxipassenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiPassengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
