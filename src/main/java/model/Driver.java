4b899d76ab44137cbd0d8c56c3083a61f2d3d195
DAHAB <abdulhadi1986 / taxi-passenger-service>
2021-06-17 18:01:05 +0300

    add class driver

---
+++ src/main/java/model/Driver.java
@@ -0,0 +1,65 @@
+package model;
+
+public class Driver {
+    private String firstName;
+    private String lastName;
+    private String phonNum;
+    private String email;
+    private String userName;
+    private String password;
+
+
+    public Driver(){};
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhonNum() {
+        return phonNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhonNum(String phonNum) {
+        this.phonNum = phonNum;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+
+}
